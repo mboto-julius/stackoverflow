bc54c23523a9f9d379254bf3871b6057e8e0e913
Julius Mboto <juliusmboto@gmail.com>
2024-08-23 13:38:52 +0300

    feat: implement deletion of the best answer

--- app/Models/Answer.php
+++ app/Models/Answer.php
@@ -29,6 +29,11 @@ class Answer extends Model
         return $this->created_at->diffForHumans();
     }
 
+    public function getStatusAttribute()
+    {
+        return $this->id === $this->question->best_answer_id ? 'vote-accepted' : '';
+    }
+    
     public static function boot()
     {
         parent::boot();
@@ -37,7 +42,7 @@ class Answer extends Model
         });
         static::deleted(function ($answer) {
             $question = $answer->question;
-            $answer->question->decrement('answers_count');
+            $question->decrement('answers_count');
             if($question->best_answer_id === $answer->id){
                 $question->best_answer_id = NULL;
                 $question->save();
